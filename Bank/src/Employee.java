b42a5b02f6cecd5dc80ebeb7b711e08df9dd3297
omkharche <omkharche321@gmail.com>
2020-06-26 11:46:53 +0530

    code added in current class and new class employee added

---
+++ Bank/src/Employee.java
@@ -0,0 +1,28 @@
+
+public class Employee {
+	protected String companyName;
+	protected String companyAddress;
+	protected int noOfEmployees;
+	public String getCompanyName() {
+		return companyName;
+	}
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+	public String getCompanyAddress() {
+		return companyAddress;
+	}
+	public void setCompanyAddress(String companyAddress) {
+		this.companyAddress = companyAddress;
+	}
+	public int getNoOfEmployees() {
+		return noOfEmployees;
+	}
+	public void setNoOfEmployees(int noOfEmployees) {
+		this.noOfEmployees = noOfEmployees;
+	}
+	
+	
+	
+
+}
